1b89c956a3e8a7ed7c49ed88906a286f3a13b23b
Supragyan Pattanayak <supragyan@live.com>
2026-05-06 00:04:47 +0530

    reverse sentence modifed and added example from medium

--- src/main/java/reverseWords.java
+++ src/main/java/reverseWords.java
@@ -17,14 +17,44 @@ public class reverseWords {
             start++;
             end--;
         }
-        String newSentence = String.join(" ", arr);
+        String newSentence = String.join(" ", arr).trim();
         return newSentence;
 
     }
 
     public static void main(String[] args){
         String sentence = "a fox was running";
+        String sentence1 = "a truck was moving at a very high speed on a narrow road.";
+        String sentence2 = "word";
         // int[] arr = {1,2,3,4,5};
         System.out.println((reverseSen(sentence)));
+        System.out.println((reverseSen(sentence1)));
+        System.out.println((reverseSen(sentence2)));
     }
 }
+
+/*
+public class ReverseWords {
+    public static void main(String[] args) {
+        System.out.println(revWords("apple banana kiwi")); // kiwi banana apple
+        System.out.println(revWords("I am John Doe")); // Doe John am I
+        System.out.println(revWords("orange")); // orange
+    }
+
+    public static String revWords(String str) {
+        StringBuilder res = new StringBuilder();
+
+        // split input string by " " space to get each word as String[]
+        String[] words = str.split(" ");
+
+        // loop over the array from back
+        for(int i = words.length - 1; i >= 0; i--) {
+            // add words to res with space
+            res.append(words[i]).append(" ");
+        }
+
+        // trim needed to remove last space in the end
+        return res.toString().trim();
+    }
+}
+*/
