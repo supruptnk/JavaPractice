6d196c445c0decec0361a62e3cadcc5218ed12ec
Supragyan Pattanayak <supragyan@live.com>
2026-05-09 23:50:48 +0530

    updated max array for loop

--- src/main/java/MaxFromArray.java
+++ src/main/java/MaxFromArray.java
@@ -9,9 +9,9 @@ public class MaxFromArray {
     public static int findMax(int[] arr){
         int max=arr[0];
 
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
+        for(int i:arr){
+            if(i>max){
+                max=i;
             }
             
         }
