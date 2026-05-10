abb53ad44f014ff5e70f1c1c46996aea2313fa7b
Supragyan Pattanayak <supragyan@live.com>
2026-05-10 23:27:07 +0530

    find second min in array

--- src/main/java/SecondMinMaxFromArray.java
+++ src/main/java/SecondMinMaxFromArray.java
@@ -1,9 +1,10 @@
 public class SecondMinMaxFromArray {
     public static void main(String[] args){
-        System.out.println(secondMax(new int[]{1,2,3,4,5,6,7,6,5,3,6}));
+        System.out.println(findSecondMax(new int[]{1,2,3,4,5,6,7,6,5,3,6}));
+        System.out.println(findSecondMin(new int[]{1,2,3,4,5,6,7,6,5,3,6}));
     }
 
-    public static int secondMax(int[] arr){
+    public static int findSecondMax(int[] arr){
         int max = arr[0];
         int maxIndex = 0;
         for(int i =0; i<arr.length; i++){
@@ -24,4 +25,30 @@ public class SecondMinMaxFromArray {
         }
         return secondMax;
     }
+
+    public static int findSecondMin(int[] arr){
+        int min = arr[0];
+        int minIndex = 0;
+        for(int i =0; i<arr.length; i++){
+            
+            if(min >arr[i]){
+                min=arr[i];
+                minIndex= i;
+            }
+        }
+
+        int secondMin = Integer.MAX_VALUE;
+        for(int i =0; i<arr.length; i++){
+            
+            if(i!=minIndex && secondMin >arr[i]){
+                // System.out.println("post secondmin: "+secondMin+" ; number:"+arr[i]);
+                secondMin=arr[i];
+                // System.out.println("post secondmin: "+secondMin+" ; number:"+arr[i]);
+            }
+        }
+        return secondMin;
+    }
+
 }
+
+
