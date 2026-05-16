0a0a58d95756d4a9e9803cc089a69ba076025e77
Supragyan Pattanayak <supragyan@live.com>
2026-05-17 01:50:02 +0530

    updated fibonacii

--- src/main/java/calculateFibonacii.java
+++ src/main/java/calculateFibonacii.java
@@ -1,3 +1,34 @@
+//fibonacii with recursion
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class calculateFibonacii {
     
+
+    public static void main(String[] args){
+        int iterations = 10;
+        int[] arr = new int[iterations];
+        // List<Integer> arr = new ArrayList<Integer>();
+        int i =0;
+        while(i<iterations){
+            if(i<2){
+                arr[i]=1;
+            }else{
+                int x = nextNumber(arr[i-1], arr[i-2]);
+                arr[i]=x;
+            }
+            i++;
+        }
+    System.out.println("Fibonacii for "+iterations+" iterations is: ");
+        for(int j: arr){
+            System.out.print(j+", ");
+
+        }
+    }
+
+    public static int nextNumber(int senior, int superSenior){
+        return (senior+superSenior);
+        
+    }
 }
